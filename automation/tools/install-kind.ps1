fb542c11c3d330625792411a3918657afbe0ad1d
Edgar Martinez <edgar.gbg@outlook.com>
2021-03-25 16:33:16 -0700

    Made it so that installation of kind doesnt fail due to ssl errors.

--- automation/tools/install-kind.ps1
+++ automation/tools/install-kind.ps1
@@ -1,3 +1,5 @@
-invoke-webrequest https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64 -outfile ./kind
+apt-get install wget -y;
+
+wget https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64 -O ./kind --no-check-certificate
 chmod +x ./kind
 mv ./kind /usr/local/bin/kind
